b3d1a5a8070de3c1f4b27ea94557b3defee3dcf4
anaguilar02 <isaagui2002@gmail.com>
2025-03-02 16:12:25 -0600

    admin, userview and grant access

---
+++ src/app/(protected)/admin/grantaccess/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainPage: React.FC = () => {
+  return (
+    <div className="text-black p-4">
+      <h1 className="text-[2rem] font-bold">ITSM Admin</h1>
+      <p className="text-[1.25rem]">Welcome to your Grant Access Functionality.</p>
+    </div>
+  );
+};
+
+export default MainPage;
