eab25732ebb9846988d1a96a306199beaecd026d
anaguilar02 <isaagui2002@gmail.com>
2025-03-04 19:24:19 -0600

    grant users, db users' data extraction

--- src/hooks/db.js
+++ src/hooks/db.js
@@ -1,4 +1,4 @@
-import { collection, getDocs, getFirestore, Firestore } from "firebase/firestore";
+import { collection, getDocs, getFirestore, Firestore, query, where } from "firebase/firestore";
 import { app } from "../firebaseConfig";
 
 const db = getFirestore(app);
@@ -8,4 +8,11 @@ export async function getEmails() {
     const usersSnapshot = await getDocs(usersCol);
     const emails = usersSnapshot.docs.map(doc => doc.data()["email"]);
     return emails;
+  }
+
+export async function getUsersData() {
+    const usersCol = collection(db, "Users");
+    const usersSnapshot = await getDocs(usersCol);
+    const usersData = usersSnapshot.docs.map(doc => doc.data());
+    return usersData;
   }
