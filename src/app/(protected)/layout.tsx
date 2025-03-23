df87ae930d849c697409dc4cfa792ff2b578491a
William <william.yuhe@gmail.com>
2025-03-22 20:37:46 -0600

    UI change for multi-role users, only test

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -12,6 +12,8 @@ import { auth } from '../../firebaseConfig';
 import { getCurrUserData } from '../../hooks/db';
 import { useAuth } from '../../hooks/useAuth';
 import { createCookie } from "@/hooks/cookies";
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -34,7 +36,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
             const data = await getCurrUserData();
             if(data)
             {
-                const name = data["name"];
+                const name = data["name"] + " " + data["last_name_1"] + " " + data["last_name_2"];
                 const img = data["picture_url"];
                 setUserData({name, img});
                 setIsAdmin(data["rol"] == "Admin");
@@ -236,6 +238,51 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                     <span style={{ fontWeight: "bold", color: "#1E293B" }}>
                         {userData.name}
                     </span>
+                    
+                    {isAdmin && (
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                            <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50">
+                            Change Role
+                            <ChevronDownIcon aria-hidden="true" className="-mr-1 size-5 text-gray-400" />
+                            </MenuButton>
+                        </div>
+
+                        <MenuItems
+                            transition
+                            className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white ring-1 shadow-lg ring-black/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                        >
+                            <div className="py-1">
+                            <MenuItem>
+                                <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                                >
+                                Admin
+                                </a>
+                            </MenuItem>
+                            <MenuItem>
+                                <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                                >
+                                IT Support
+                                </a>
+                            </MenuItem>
+                            <MenuItem>
+                                <a
+                                href="#"
+                                className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:text-gray-900 data-focus:outline-hidden"
+                                >
+                                General User
+                                </a>
+                            </MenuItem>
+                            </div>
+                        </MenuItems>
+                    </Menu>)}
+
+
+
                     <button
                         style={{
                         background: "#EF4444",
